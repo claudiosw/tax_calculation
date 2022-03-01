af6b5a934f0b1a79c83e9227e65d87c1c7909888
claudiosw@gmail.com <claudiosw@gmail.com>
2022-03-01 17:12:12 -0300

    improved documentation of the calculate function

--- tax.py
+++ tax.py
@@ -110,7 +110,7 @@ class Tax(object):
 
     def calculate(self):
         """
-        Get input data, calculate tax and output it
+        Get input data from the input file, calculate tax and print the receipt of the sale including tax
 
         """
         for dic in self._list_of_items:
